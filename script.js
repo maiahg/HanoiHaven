2dd457c4b469808ca91849e2f008210a164d8732
maiahg <122498165+maiahg@users.noreply.github.com>
2023-12-25 01:44:20 -0500

    Added header

---
+++ script.js
@@ -0,0 +1,8 @@
+let menu = document.querySelector('.menu-bar');
+let navbar = document.querySelector('.navbar');
+let container = document.querySelector('.container');
+
+menu.onclick = () => {
+    navbar.classList.toggle('active');
+    container.classList.toggle('active');
+}
