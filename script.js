4e461261def32af67dacc7e84393aabd33dbe5d4
maiahg <122498165+maiahg@users.noreply.github.com>
2023-12-25 17:37:19 -0500

    Added Our Story Section

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 let menu = document.querySelector('.menu-bar');
 let navbar = document.querySelector('.navbar');
 let container = document.querySelector('.container');
+const scrollDownBtn = document.querySelector('.scroll-down');
+const aboutSection = document.querySelector('#about');
 
 menu.onclick = () => {
     navbar.classList.toggle('active');
     container.classList.toggle('active');
+}
+
+scrollDownBtn.onclick = () => {
+    aboutSection.scrollIntoView({behavior: 'smooth'});
 }
